70c2ce704218407d859b686751e42a41cb58ad01
Germán A. Ramírez Vela <german.ca13@gmail.com>
2018-11-07 17:16:00 -0500

    Projects to work with String and DateTimes were added.

---
+++ MVA_CSharp_Fundamentals/WorkingWithDateTime/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace WorkingWithDateTime
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         DateTime dateTime = DateTime.Now;
+
+         //Printing the DateTime values
+         Console.WriteLine(dateTime);
+         Console.WriteLine(dateTime.ToString());
+         
+         Console.WriteLine("Date - Short and Long");
+         Console.WriteLine(dateTime.ToShortDateString());
+         Console.WriteLine(dateTime.ToLongDateString());
+
+         Console.WriteLine("Time - Short and Long");
+         Console.WriteLine(dateTime.ToShortTimeString());
+         Console.WriteLine(dateTime.ToLongTimeString());
+
+         //Adding MILLISECONDS, SECONDS, MINUTES, DAYS, MONTHS, YEARS
+         DateTime date1 = dateTime.AddDays(3);
+         DateTime date2 = dateTime.AddMonths(1);
+         DateTime dat32 = dateTime.AddYears(5);
+
+         //Creating Datetimes instances
+         DateTime birthDay = new DateTime(1991, 1, 9);
+         DateTime birthDayFromString = DateTime.Parse("1991-01-09");
+
+         //Substract time
+         //TimeSpan: It represents the elapsed time between two specific DateTimes
+         TimeSpan myAge = DateTime.Now.Subtract(birthDayFromString);
+         Console.WriteLine(myAge.TotalDays);
+
+         Console.ReadKey();
+      }
+   }
+}
---
+++ MVA_CSharp_Fundamentals/WorkingWithStrings/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+
+namespace WorkingWithStrings
+{
+   /// <summary>
+   /// Working with Strings
+   /// In this example, It's shown the different ways that we can manipulate a string variable.
+   /// The String data type is "Immutable", that is, it cannot be modified.
+   /// If this is modified, a new string is created.
+   /// </summary>
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         string myString = "This is my message for people that live in Colombia";
+         Console.WriteLine(myString);
+
+         //The Backslash (\) is the character used to scape special characters. --> \n; New Line, \r
+         string scapingCharacter = "This is my message for \"people\" that live in Colombia";         
+         Console.WriteLine(scapingCharacter);
+
+         //If we need to print the Backslash character into a string message.
+         //We must add a Backslash for each "\"
+         string backslash = "The file can be found in the following path C:\\MyFolder\\Files";
+         Console.WriteLine(backslash);
+
+         //We can use the @ character to indicate to compiler that we want to try the string as literal
+         string backslashArroba = @"The file can be found in the following path C:\MyFolder\Files";
+         Console.WriteLine(backslashArroba);
+
+         //Using the String.Format method
+         Console.WriteLine(string.Format("{0} - {1}", "First", "Second"));
+         Console.WriteLine(string.Format("{0} - {0}", "First", "Second"));
+         Console.WriteLine(string.Format("{1} - {0}", "First", "Second"));
+
+         //Currency format based on Current Culture Info
+         Console.WriteLine(string.Format("{0:C}", 12345.55));
+
+         //Number format based on Current Culture Info
+         Console.WriteLine(string.Format("{0:N}", 123456789));
+
+         //Percentage format based on Current Culture Info
+         Console.WriteLine(string.Format("{0:P}", .35));
+
+         //Custom format
+         Console.WriteLine(string.Format("{0:(###) ###-####}", 0576815797));
+
+         //Concatenating a string
+         //Each time, the CLR makes a new variable when the += operator is used.
+         //Below, it's a inefficient way to work.
+         myString = string.Empty;
+
+         for (int i = 1; i <= 100; i++)
+         {
+            myString += "--" + i;
+         }
+         Console.WriteLine(myString);
+
+         //The best way to do the above approach is using the StringBuilder class.
+         StringBuilder builder = new StringBuilder();
+
+         for (int i = 1; i <= 100; i++)
+         {
+            builder.Append("--" + i);            
+         }
+         Console.WriteLine(myString);
+
+         Console.ReadKey();
+      }
+   }
+}
